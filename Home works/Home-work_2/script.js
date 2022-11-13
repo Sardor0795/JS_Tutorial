aff80f36e3cb804685eb30aaefe54a8b4e0ebcf6
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-11-13 19:21:20 +0500

    Make hometask 3 more readable

--- Home works/Home-work_2/script.js
+++ Home works/Home-work_2/script.js
@@ -20,28 +20,28 @@
 
 // Task 3 --------------------------------------------------------
 
-"" + 1 + 0 // natija 10 (string) chiqadi sababi "" dan keyingi xammasi string tipiga o'tip ketadi
+"" + 1 + 0       // natija 10 (string) chiqadi sababi "" dan keyingi xammasi string tipiga o'tip ketadi
 
-"" - 1 + 0 // natija -1 (number) chiqadi sababi "" dan keyin stringga o'tgani blan - dan keyin kelgan string raqamlar number tipiga o'tib ketadi
+"" - 1 + 0       // natija -1 (number) chiqadi sababi "" dan keyin stringga o'tgani blan - dan keyin kelgan string raqamlar number tipiga o'tib ketadi
 
-true + false // natija 1 (number) chiqadi sababi true 1 ga false 0 ga teng
+true + false     // natija 1 (number) chiqadi sababi true 1 ga false 0 ga teng
 
-6 / '3' // natija 2 (number) chiqadi sababi (/) stringni numberga o'tkazip oladi
+6 / '3'          // natija 2 (number) chiqadi sababi (/) stringni numberga o'tkazip oladi
 
-'2' * '2' // natija 4 (number) chiqadi sababi (*) ikkala stringni ham numberga o'tkazip oladi
+'2' * '2'        // natija 4 (number) chiqadi sababi (*) ikkala stringni ham numberga o'tkazip oladi
 
-4 + 5 + "px" // natija 9px (string) chiqadi. Birinchi 4 va 5 matematik qo'shilip 9 bo'ladi va oxiridan "px" (string) qo'shilip 9px (string) bo'ladi va tipixam stringga o'zgaradi
+4 + 5 + "px"     // natija 9px (string) chiqadi. Birinchi 4 va 5 matematik qo'shilip 9 bo'ladi va oxiridan "px" (string) qo'shilip 9px (string) bo'ladi va tipixam stringga o'zgaradi
 
-"$" + 4 + 5 // natija $45 (string) chiqadi sababi string o'zidan boshlab qolgan xammasini string tipiga o'tqazvoradi
+"$" + 4 + 5      // natija $45 (string) chiqadi sababi string o'zidan boshlab qolgan xammasini string tipiga o'tqazvoradi
 
-"4px" - 2 // natija NaN chiqadi sababi (-) 'px' ni raqamga aylantira olmaydi
+"4px" - 2        // natija NaN chiqadi sababi (-) 'px' ni raqamga aylantira olmaydi
 
-"  -9  " + 5 // (  -9  5) (string) chiqadi. String ichidagi bo'sh joylar xisobga olinadi va o'zidan keyingi 5(number) nixam string tipiga o'tkazvoradi
+"  -9  " + 5     // (  -9  5) (string) chiqadi. String ichidagi bo'sh joylar xisobga olinadi va o'zidan keyingi 5(number) nixam string tipiga o'tkazvoradi
 
-"  -9  " - 5 // javob -14 (number) chiqadi sababi (-) string ichidagi bo'sh joylarni o'chiradi va string -9 ni numberga o'tqazip undan -5 (number) ni ayiradi natijada -14 xosil bo'ladi
+"  -9  " - 5     // javob -14 (number) chiqadi sababi (-) string ichidagi bo'sh joylarni o'chiradi va string -9 ni numberga o'tqazip undan -5 (number) ni ayiradi natijada -14 xosil bo'ladi
 
-null + 1 // natija 1 (number) chiqadi sababi null 0 ga teng
+null + 1         // natija 1 (number) chiqadi sababi null 0 ga teng
 
-undefined + 1 // natija NaN chiqadi sababi undefined xech qanday raqamga teng emas va u raqamlarga umuman aloqasi yo'q (undefined faqat == bilan null ga taqqoslaganda teng va boshqa xech narsaga teng emas)
+undefined + 1    // natija NaN chiqadi sababi undefined xech qanday raqamga teng emas va u raqamlarga umuman aloqasi yo'q (undefined faqat == bilan null ga taqqoslaganda teng va boshqa xech narsaga teng emas)
 
-" \t \n " - 2 // natija -2 (number) chiqadi sababi (-) tab va new line dagi barcha bo'sh joylarni kesip tashaydi va 2 number tipida - ga o'tib faqat o'zi qoladi
+" \t \n " - 2    // natija -2 (number) chiqadi sababi (-) tab va new line dagi barcha bo'sh joylarni kesip tashaydi va 2 number tipida - ga o'tib faqat o'zi qoladi
