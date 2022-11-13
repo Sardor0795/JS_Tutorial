8cab247fc81ceecf59137c60d6a5729b28392725
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-11-13 18:34:56 +0500

    Add 2nd home task

---
+++ Home works/Home-work_2/script.js
@@ -0,0 +1,16 @@
+// Task 1
+
+// let a = 1, b = 1;
+
+// let c = ++a // c == 2 (c ning qiymati 2 ga teng bo'ladi -> sababi ++a birinchi qiymatini 1 ga oshiradi va uni c ga beradi)
+// let d = b++ // d == 1 (d ning qiymati 1 ga teng bo'ladi -> sababi a++ birinchi o'zidagi qiymatni c ga beradi keyin 1 ga oshadi)
+
+// console.log(c); // 2
+// console.log(d); // 1
+// console.log(d); // 1
+
+// Task 2
+
+// let a = 2;
+
+// let x = 1 + (a *= 2); // 5 (Javob 5 chiqadi sababi -> (a *= 2) (a = a * 2) ga teng)
