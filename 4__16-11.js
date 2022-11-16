da6d14385a47490b1172c5c3e95a4a4e76b98f37
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-11-16 17:54:46 +0500

    Switch statement, For Loops

---
+++ 4__16-11.js
@@ -0,0 +1,66 @@
+// Switch statement
+
+// Same as If, but waits only exact value (===)
+
+// let grade = "best";
+
+// switch (grade) {
+//   case "best":
+//     console.log("5");
+//     break;
+//   case "good":
+//     console.log("4");
+//     break;
+//   case "satisfactory":
+//     console.log("3");
+//   case "2":
+//     console.log("bad");
+//     break;
+//   default:
+//     console.log("Enter only number");
+// }
+
+// We need break to stop if statement true
+// In If else statement === default in switch
+
+// For Loops
+// Basic for loop
+
+// for loop
+// - for (begin; condition; step)
+// - for (; i < 3; i++) {} // 0, 1, 2
+// - for (; i < 3;) {}
+// - for (;;) {} run without limits
+// break, continue
+
+// break stops hole codes
+// continue skips one step
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// Nested loop
+
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     console.log(j);
+//   }
+// }
+
+// If statement in For loop
+
+// for (let i = 1; i < 10; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// Labeling loop
+
+// outer: for (let i = 1; i <= 10; i++) {
+//   inner: for (let j = 1; j <= i; j++) {
+//     console.log(j);
+//     if (i === 5) break outer;
+//   }
+// }
