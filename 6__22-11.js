80bd9e41a39b29937cff750127cf480c83a41546
Sardor0795 <english.programmer.9595@gmail.com>
2022-11-22 18:35:43 +0500

    Objects

---
+++ 6__22-11.js
@@ -0,0 +1,57 @@
+// Objects
+
+// - Creating objects in sevral ways { ... }
+
+    // - object literals - let obj = {}
+    // - object constructor - let obj = new Object()
+    // { key : value }
+    // key -> name -> identifier -> property
+
+        // let obj = {
+        //     name: 'Jim',
+        //     age: 18
+        // }
+
+    // accessing keys with dots, variables, []
+
+        // obj.name
+        // obj['name']
+
+    // multiword property and numeric -> obj[0] === pbj['0']
+
+        // obj['user name']
+
+    // [],.
+
+        // [] dynamic. take value even variables
+
+    // delete
+
+        // delete obj.name
+
+    // make dynamic user with return function
+    // abbrivation with keys and variables
+
+        // if in obj key and value the same name we can write one
+
+            // let obj = {
+            //     name,
+            //     age
+            // }
+
+    // in -> "age" in user// true
+
+        // age in user -> returns boolean
+
+    // for loop with in // prints key
+
+        // for (let i in obj) {
+        //     console.log(i);
+        // }
+
+// - Reference and copy
+    // copy obj vs copy number
+    // comparision reference and value objs
+    // Object.assign(dest, source)
+
+    // -structuredClone - clone only values not ref
