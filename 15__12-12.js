322be417ed6cc1cb666d8ec8e88c2da452657d64
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2022-12-20 18:46:46 +0500

    Closure, Lexical Environment, Scope

---
+++ 15__12-12.js
@@ -0,0 +1,30 @@
+// Scope, closure, lexical environment
+
+// {
+//   let car = "cadilac";
+// }
+
+// console.log(car);
+
+// for (let index = 0; index < array.length; index++) {
+//   const element = array[index];
+// }
+
+// console.log(index);
+
+// let car = "cadilac";
+
+// function name(params) {
+//   console.log(car);
+// }
+
+// name();
+
+// let car = "cadilac";
+
+// function name(params) {
+//   console.log(car);
+//   let car2 = "BMW";
+// }
+
+// name();
