763315b168fb87666f3008e5a71de211a2c09383
Sardor Tursunboyev <english.programmer.9595@gmail.com>
2023-01-07 15:03:05 +0500

    DOM-2

---
+++ 26__06-01.js
@@ -0,0 +1,55 @@
+let p = document.querySelector("p");
+let body = document.querySelector("body");
+
+// Attributes
+
+// console.log(p.hasAttribute("class"));
+// console.log(p.getAttribute('class'));
+// p.setAttribute('name', 'attr')
+// p.removeAttribute('name')
+
+// Elements
+
+// let div = document.createElement("div");
+// let txt = document.createTextNode("some text");
+
+// div.append(txt)
+// body.prepend(div)
+// p.replaceWith(div)
+
+// p.append('<p>Some txt</p>')
+
+// Insertion
+
+// append
+// prepend
+// before
+// after
+// replaceWith
+
+// insertAdjacentHTML('where', elm)
+
+// beforebegin
+// afterbegin
+// beforeend
+// afterend
+
+// p.insertAdjacentHTML('afterend', '<div>Some txt</div>')
+
+// Remove element
+
+// p.remove()
+
+// let p2 = p.cloneNode(false) //true-false
+// body.prepend(p2)
+
+// Element styles ---
+
+// p.style.backgroundColor = "red";
+// console.log(p.className);
+// console.log(p.classList.contains('ggg')); // add, remove, toggle, contains
+
+// p.className = 'ggg'
+// p.classList.add("ggg");
+
+// console.log(p.getBoundingClientRect());
