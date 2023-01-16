769a907134955a26772af36f892eebc693ef1f87
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-16 11:16:42 +0500

    DOM events

---
+++ 29__16-01.js
@@ -0,0 +1,77 @@
+// box.oncontextmenu = () => {
+//   console.log("oncontextmenu");
+// };
+
+// box.onmouseover = () => {
+//   console.log("mouse over");
+// };
+
+// box.onmouseout = () => {
+//   console.log("mouse out");
+// };
+
+let box = document.getElementById("box");
+let input = document.getElementById("input");
+
+// input.onkeydown = (e) => {
+//   //   box.textContent += e.keyCode;
+//   //   console.log(e.key);
+//   //   console.log(e.type);
+// };
+
+// box.onclick = (e) => {
+//   if (e.altKey) console.log(5);
+// };
+
+// box.onclick = (e) => {
+//   if (e.shiftKey) console.log(5);
+// };
+
+// box.onclick = (e) => {
+//   if (e.ctrlKey) console.log(5);
+// };
+
+// box.onclick = (e) => {
+//   if (e.metaKey) console.log(5);
+// };
+
+// box.onclick = (e) => {
+//   if (e.ctrlKey && e.altKey) console.log(5);
+// };
+
+// box.onclick = (e) => {
+//   console.log(e.ctrlKey);
+// };
+
+// input.onkeydown = () => {
+//   box.innerHTML = input.value;
+// };
+
+// input.onchange = () => {
+//   box.innerHTML = input.value;
+// };
+
+// input.onkeypress = () => {
+//   box.innerHTML = input.value;
+// };
+
+// box.ondblclick = () => {
+//   console.log(5);
+// };
+
+// input.oncopy = (e) => {
+//   e.preventDefault();
+// };
+
+// input.onpaste = (e) => {
+//   e.preventDefault();
+// };
+
+// console.log(box.scrollHeight);
+// console.log(box.clientHeight);
+
+// box.onscroll = () => {
+//   if (box.scrollTop + box.clientHeight + 50 > box.scrollHeight) {
+//     box.innerHTML += box.innerHTML;
+//   }
+// };
