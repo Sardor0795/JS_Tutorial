8628b4301860d7cd3a2429c17d3866be198c0c00
Sardor0795 <english.programmer.9595@gmail.com>
2023-01-23 21:06:31 +0500

    DOM Forms

---
+++ 30__23-01.js
@@ -0,0 +1,19 @@
+/*
+
+DOM - Forms
+
+form action request to back
+form name = document.forms[0] or .name
+
+let form = document.forms.name
+
+form.select
+
+
+input focus/blur
+oninput
+
+form method PUT,Post
+
+
+*/
