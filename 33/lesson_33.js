a40b12acf023a418d7857f302296375d75c6fd2c
Sardor0795 <english.programmer.9595@gmail.com>
2023-02-01 20:32:47 +0500

    WebSocket

--- 33/lesson_33.js
+++ 33/lesson_33.js
@@ -1,16 +1,12 @@
 let msgInp = document.querySelector(".msg_inp");
 let msgWrapper = document.querySelector(".msg");
 
-let socket = new WebSocket("ws://127.0.0.1:3000/33/index.html");
+let socket = new WebSocket("ws://127.0.0.1:2000/33/index.html");
 
 socket.onmessage = (e) => {
-  let div = document.createElement("div");
-  div.innerText = e?.data;
-  msgWrapper.append(div);
+  msgWrapper.innerHTML += `<div>${e.data}</div>`;
 };
 
 msgInp.onblur = () => {
   socket.send(msgInp.value);
-  msgInp.value = "";
-  console.log('Message sent');
 };
--- 33/server.js
+++ 33/server.js
@@ -1,17 +1,22 @@
 const WebSocet = require("ws");
 
-let ws = new WebSocet.Server({ port: 3000 });
+let ws = new WebSocet.Server({ port: 2000 });
 
 ws.on("connection", (w, req) => {
+  w.onmessage = (e) => {
+    ws.clients.forEach((client) => {
+      client.send(e.data);
+      console.log(e.data);
+    });
+  };
+});
+
+/*
+
   console.log("Connected...");
 
   w.on("close", (w) => {
     console.log("Disconnected");
   });
 
-  w.onmessage = (e) => {
-    ws.clients.forEach((client) => {
-      client.send(e.data);
-    });
-  };
-});
+*/
